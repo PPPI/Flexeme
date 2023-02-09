4cfae123b365d82407597ce81da96e76a3b273f5
Thomas Schweizer <tschweiz@cs.washington.edu>
2023-02-08 18:55:47 -0800

    Support any node labelling generated by the PDG extractors

--- src/deltaPDG/deltaPDG.py
+++ src/deltaPDG/deltaPDG.py
@@ -1,5 +1,7 @@
 from typing import List, Tuple
 
+import networkx as nx
+
 from .Util.mark_pdgs import mark_pdg_nodes
 from .Util.merge_marked_pdgs import Marked_Merger
 from .Util.pygraph_util import read_graph_from_dot, obj_dict_to_networkx
@@ -7,12 +9,17 @@ from .Util.pygraph_util import read_graph_from_dot, obj_dict_to_networkx
 
 class deltaPDG(object):
     def __init__(self, base_pdg_location: str, m_fuzziness: int, n_fuzziness: int):
-        self.before_pdg = obj_dict_to_networkx(read_graph_from_dot(base_pdg_location))
+        self.before_pdg = self.reset_nodes_labels(obj_dict_to_networkx(read_graph_from_dot(base_pdg_location)))
         self.merger = Marked_Merger(m_fuzziness=m_fuzziness, n_fuzziness=n_fuzziness)
 
     def __call__(self, target_pdg_location: str, diff: List[Tuple[str, str, int, int, str]]):
-        after_pdg = obj_dict_to_networkx(read_graph_from_dot(target_pdg_location))
+        after_pdg = self.reset_nodes_labels(obj_dict_to_networkx(read_graph_from_dot(target_pdg_location)))
         marked_before = mark_pdg_nodes(self.before_pdg, '-', diff)
         marked_after = mark_pdg_nodes(after_pdg, '+', diff)
         self.deltaPDG = self.merger(before_apdg=marked_before, after_apdg=marked_after)
         return self.deltaPDG
+
+    def reset_nodes_labels(self, pdg):
+        pdg_integers = nx.convert_node_labels_to_integers(pdg)
+        pdg_reset = nx.relabel_nodes(pdg_integers, lambda n: 'n' + str(n))
+        return pdg_reset
